6c21fce21459bb4426ec75e7a572b00778a6d640
AkashSharma-2023 <sharmaakashrock2023@gmail.com>
2023-07-21 16:10:42 +0530

    i  want to add

---
+++ fibonacici.py
@@ -0,0 +1,17 @@
+# The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones
+# starting from 0 and 1.
+
+def fib(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+    for i in range(2,n):
+        c = a + b
+        a = b
+        b = c
+        print(c)
+fib(10)
