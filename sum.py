7e3a36f6aaf89bd66f06dad207123cdef09f8e1f
AkashSharma-2023 <sharmaakashrock2023@gmail.com>
2023-07-21 16:07:25 +0530

    i  want to add

---
+++ sum.py
@@ -0,0 +1,8 @@
+#write a python function that takes a list of numbers as input and returns the sum os all the numbers in the list
+first = input("enter first number : ")
+second = input("enter second number : ")
+sum = int(int(first) + int(second))
+print("the sum is : " + str(sum))
+
+str = input("hello"[::-1])
+print(str)
