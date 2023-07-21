af72a1165384f2dd3ca8d832f4393cdeff91bb97
AkashSharma-2023 <sharmaakashrock2023@gmail.com>
2023-07-21 16:12:00 +0530

    i  want to add

--- fibonacici.py
+++ fibonacici.py
@@ -1,3 +1,4 @@
+#Write a Python function that generates the first n numbers of the Fibonacci sequence, where n is taken as input.
 # The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones
 # starting from 0 and 1.
 
